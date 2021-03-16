edf0a8025a02c39dabcd38f0e5cb617b25da8edf
voanhqt <anhvodbu@gmail.com>
2021-03-16 20:45:09 +0700

    kiểm tra chuỗi có kí tự trắng hay không

---
+++ login.php
@@ -0,0 +1,14 @@
+<form action="checkaccount.php" method="post">
+    <div data-reactid="131">
+        <input type="text" name="username" value="" placeholder="Tên đăng nhập" class="_2K5yV _34Elg" data-reactid="132">
+        <span class="_2SZYG">Vui lòng nhập tài khoản</span>
+    </div>
+    <div data-reactid="131">
+        <input type="password" name="password" value="" placeholder="mật khẩu" class="_2K5yV _34Elg" data-reactid="132">
+        <span class="_2SZYG">Vui lòng nhập tài khoản</span>
+    </div>
+    
+    <div>
+        <input type="submit" value="OK">
+    </div>
+</form>
